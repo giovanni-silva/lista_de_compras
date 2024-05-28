7b98803da65d21f36c329b9199e7d25d7d25e448
Giovanni Bruce da Silva <90220454+giovanni-silva@users.noreply.github.com>
2024-05-28 20:12:00 -0300

    service ok

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
             .then(cache => {
                 return cache.addAll([
-                    '/lista_de_compras/', // Mudança aqui: Incluindo a raiz do PWA
+                    '/lista_de_compras/', // Raiz do PWA
                     '/lista_de_compras/index.html',
                     '/lista_de_compras/styles.css',
                     '/lista_de_compras/script.js',
@@ -25,7 +25,36 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return response || fetch(event.request);
+                // Retorna a resposta do cache, se disponível
+                if (response) {
+                    return response;
+                }
+
+                // Se a resposta não estiver em cache, faz uma requisição de rede
+                return fetch(event.request)
+                    .then(response => {
+                        // Abre o cache e armazena a nova resposta
+                        return caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, response.clone());
+                                return response;
+                            });
+                    });
             })
+    );
+});
+
+// Atualiza o cache quando uma nova versão do Service Worker estiver disponível
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
