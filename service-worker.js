2d645af6d1731189e23161ac660f685f0fdb4c84
Giovanni Bruce da Silva <90220454+giovanni-silva@users.noreply.github.com>
2024-05-28 20:04:55 -0300

    service feature

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
 const CACHE_NAME = 'lista_de_compras-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/manifest.json',
-    '/carrinho_celular.png',
-    '/carrinho_desktop.png',
-    '/favicon.png'  // Adicionado favicon ao cache
-];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                return cache.addAll(urlsToCache);
+                return cache.addAll([
+                    '/lista_de_compras/', // Mudança aqui: Incluindo a raiz do PWA
+                    '/lista_de_compras/index.html',
+                    '/lista_de_compras/styles.css',
+                    '/lista_de_compras/script.js',
+                    '/lista_de_compras/manifest.json',
+                    '/lista_de_compras/carrinho_celular.png',
+                    '/lista_de_compras/carrinho_desktop.png',
+                    '/lista_de_compras/favicon.png'
+                ]);
+            })
+            .catch(error => {
+                console.error('Erro ao adicionar recursos ao cache:', error);
             })
     );
 });
